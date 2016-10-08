ff74605eeca607ee152ebb67d330c5d3a2b9c888
sky_lock <sho59sky@outlook.jp>
2016-10-08 15:05:17 +0900

    Revert 95957c5 commit.

--- src/main/java/mc/sky_lock/jumppad/InteractListener.java
+++ src/main/java/mc/sky_lock/jumppad/InteractListener.java
@@ -12,9 +12,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * @author sky_lock
@@ -51,36 +48,11 @@ class InteractListener implements Listener {
             return;
         }
         Sign sign = (Sign) underBlock.getState();
-        List<String> strs= new ArrayList<>();
+        List<String> signStrs = new ArrayList<>();
 
-        Arrays.asList(sign.getLines()).forEach(strs::add);
-        new BukkitRunnable() {
+        Arrays.asList(sign.getLines()).forEach(signStrs::add);
 
-            @Override
-            public void run() {
-                if (strs.size() < 3) {
-                    return;
-                }
-                if (!(strs.get(0).equalsIgnoreCase("[jumppad]"))) {
-                    return;
-                }
-                double scalar = isDouble(strs.get(1)) ? Double.parseDouble(strs.get(1)) : 3.0;
-                double YAxis = isDouble(strs.get(2)) ? Double.parseDouble(strs.get(2)) : 3.0;
-
-                Location loc = player.getLocation();
-
-                player.setVelocity(loc.getDirection().multiply(scalar).setY(YAxis));
-                player.playSound(loc, Sound.ENTITY_BLAZE_SHOOT, 1F, 1F);
-            }
-        }.runTaskLater(pl, 1L);
+        new JumpTask(player, signStrs).runTaskLater(pl, 1L);
     }
 
-    private boolean isDouble(String str) {
-        try {
-            Double.parseDouble(str);
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-        return true;
-    }
 }
