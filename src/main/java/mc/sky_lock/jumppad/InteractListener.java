e74fa165538fed9df4428cfcea817f3f9d674fc3
sky_lock <sho59sky@outlook.jp>
2016-11-07 19:33:30 +0900

    1Fから1.0Fに表記を変更

--- src/main/java/mc/sky_lock/jumppad/InteractListener.java
+++ src/main/java/mc/sky_lock/jumppad/InteractListener.java
@@ -1,6 +1,8 @@
 package mc.sky_lock.jumppad;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -8,13 +10,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * @author sky_lock
@@ -52,25 +52,22 @@ class InteractListener implements Listener {
         }
         Sign sign = (Sign) underBlock.getState();
         List<String> strs = new ArrayList<>();
-
         Arrays.asList(sign.getLines()).forEach(strs::add);
+        
         new BukkitRunnable() {
 
             @Override
             public void run() {
-                if (strs.size() < 3) {
-                    return;
-                }
                 if (!(strs.get(0).equalsIgnoreCase("[jumppad]"))) {
                     return;
                 }
-                double scalar = isDouble(strs.get(1)) ? Double.parseDouble(strs.get(1)) : 3.0;
-                double YAxis = isDouble(strs.get(2)) ? Double.parseDouble(strs.get(2)) : 3.0;
+                double scalar = isDouble(strs.get(1)) ? Double.parseDouble(strs.get(1)) : 3.0D;
+                double YAxis = isDouble(strs.get(2)) ? Double.parseDouble(strs.get(2)) : 3.0D;
 
                 Location loc = player.getLocation();
 
                 player.setVelocity(loc.getDirection().multiply(scalar).setY(YAxis));
-                player.playSound(loc, Sound.ENTITY_BLAZE_SHOOT, 1F, 1F);
+                player.playSound(loc, Sound.ENTITY_BLAZE_SHOOT, 1.0F, 1.0F);
             }
         }.runTaskLater(pl, 1L);
     }
